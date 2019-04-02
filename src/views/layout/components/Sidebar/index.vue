74524bf055b026bcd97e2a8bdad66b1f80fcb567
duanmw <duanmw@live.com>
2019-04-02 13:26:18 +0800

    add sidebar logo

--- src/views/layout/components/Sidebar/index.vue
+++ src/views/layout/components/Sidebar/index.vue
@@ -1,40 +1,46 @@
 <template>
-  <el-scrollbar wrap-class="scrollbar-wrapper">
-    <el-menu
-      :default-active="$route.path"
-      :collapse="isCollapse"
-      :background-color="variables.menuBg"
-      :text-color="variables.menuText"
-      :active-text-color="variables.menuActiveText"
-      :collapse-transition="false"
-      mode="vertical"
-    >
-      <sidebar-item v-for="route in permission_routers" :key="route.path" :item="route" :base-path="route.path"/>
-    </el-menu>
-  </el-scrollbar>
+  <div :class="{'has-logo':true}">
+    <logo :collapse="isCollapse"/>
+    <el-scrollbar wrap-class="scrollbar-wrapper">
+      <el-menu
+        :default-active="$route.path"
+        :collapse="isCollapse"
+        :background-color="variables.menuBg"
+        :text-color="variables.menuText"
+        :active-text-color="variables.menuActiveText"
+        :collapse-transition="false"
+        mode="vertical"
+      >
+        <sidebar-item
+          v-for="route in permission_routers"
+          :key="route.path"
+          :item="route"
+          :base-path="route.path"
+        />
+      </el-menu>
+    </el-scrollbar>
+  </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import variables from '@/styles/variables.scss'
-import SidebarItem from './SidebarItem'
+import { mapGetters } from "vuex";
+import variables from "@/styles/variables.scss";
+import SidebarItem from "./SidebarItem";
+import Logo from "./Logo";
 
 export default {
-  components: { SidebarItem },
+  components: { SidebarItem, Logo },
   computed: {
-    ...mapGetters([
-      'permission_routers',
-      'sidebar'
-    ]),
+    ...mapGetters(["permission_routers", "sidebar"]),
     // routes() {
     //   return this.$router.options.routes
     // },
     variables() {
-      return variables
+      return variables;
     },
     isCollapse() {
-      return !this.sidebar.opened
+      return !this.sidebar.opened;
     }
   }
-}
+};
 </script>
